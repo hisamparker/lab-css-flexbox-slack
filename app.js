53e61d4d2a0d94784708480e2e2488b17e3ff8ed
hisamparker <hisamparker@gmail.com>
2021-03-30 17:21:40 +0200

    medium done, almost done large

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 
 const hamburger = document.querySelector('#hamburger');
 const hamburger_menu = document.querySelector('.hamburger_menu')
-const closeButton = document.querySelector('#close__hamburger_menu')
+const closeButton = document.querySelector('#close_hamburger_menu')
 
 hamburger.addEventListener('click', () => hamburger_menu.classList.toggle('is__open'));
 
